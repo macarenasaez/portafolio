733bbdc919c698acf84f926126074031ce676208
macarenasaez <makadaira@gmail.com>
2020-02-18 09:15:26 -0300

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {StartComponent} from './components/start/start.component';
+import {BiographyComponent} from './components/biography/biography.component';
+import {ProjectsComponent} from './components/projects/projects.component';
+import {SkillsComponent} from './components/skills/skills.component';
+import {ContactComponent} from './components/contact/contact.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'home',component: StartComponent},
+  {path:'start',component: StartComponent},
+  {path:'biography',component: BiographyComponent},
+  {path:'projects',component: ProjectsComponent},
+  {path:'skills',component: SkillsComponent},
+  {path:'contact',component: ContactComponent},
+  {path:'',pathMatch:'full',redirectTo:'home'},
+  {path:'**',component: StartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import{NgbModule} from '@ng-bootstrap/ng-bootstrap'
+//import {FormsModule} from '@angular/forms';
 
+//modulo de peticion http
+//import { HttpClienModule }from   '@angular/common/http';
+
+
+//rutass
 import { AppRoutingModule } from './app-routing.module';
+
+//componentes
 import { AppComponent } from './app.component';
+import { StartComponent } from './components/start/start.component';
+import { BiographyComponent } from './components/biography/biography.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    BiographyComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    //HttpClienModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
